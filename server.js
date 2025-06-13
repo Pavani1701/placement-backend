8e972a70c3c1e914d5c7b508d6f9d49429233485
Pavani Madhyannapu <110667498+Pavani1701@users.noreply.github.com>
2025-06-13 18:54:14 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const authRoutes = require('./routes/auth');
+const jobRoutes = require('./routes/Job');
+const applicationRoutes = require('./routes/Application');
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch((err) => console.error('❌ MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.use('/api/auth', authRoutes);
+app.use('/api/jobs', jobRoutes); 
+app.use('/api/applications', applicationRoutes);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
